bd900a9eb41631f29b94db6de4423fd9a652bc33
hanzalahwaheed <hanzalah.w@gmail.com>
2024-03-08 06:50:17 +0530

    auth middlewarre done

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -3,6 +3,7 @@ import z from "zod";
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcryptjs";
+import authMiddleware from "../middleware.js";
 
 const router = express.Router();
 
@@ -55,12 +56,60 @@ router.post("/signin", async (req, res) => {
   if (!user)
     return res.status(401).json({ error: "Invalid email or password" });
 
-  const passwordMatch = await bcrypt.compare(data.password, user.password);
+  const passwordMatch = bcrypt.compare(data.password, user.password);
   if (!passwordMatch)
     return res.status(401).json({ error: "Invalid email or password" });
 
   const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
   res.json({ token });
+});
+
+const updateUserBody = z.object({
+  password: z.string().optional(),
+  firstName: z.string().optional(),
+  lastName: z.string().optional(),
+});
+
+router.put("/", authMiddleware, async (req, res) => {
+  const { success } = updateUserBody.safeParse(req.body);
+  if (!success)
+    res.status(411).json({
+      message: "Error while updating information",
+    });
+
+  try {
+    await User.updateOne({ _id: req.userId }, req.body);
+    res.status(200).json({ message: "Updated successfully" });
+  } catch (error) {
+    console.log("update", error);
+  }
+});
+
+router.get("/bulk", async (req, res) => {
+  const filter = req.query.filter || "";
+  const users = await User.find({
+    $or: [
+      {
+        firstName: {
+          $regex: filter,
+        },
+      },
+      {
+        lastName: {
+          $regex: filter,
+        },
+      },
+    ],
+  });
+
+  res.json({
+    user: users.map((user) => ({
+      username: user.username,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      _id: user._id,
+    })),
+  });
 });
 
 export default router;
