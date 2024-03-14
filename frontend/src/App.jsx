f47c8d52c2928a86704cdfdf23c41c321e0bbd41
hanzalahwaheed <hanzalah.w@gmail.com>
2024-03-14 06:17:10 +0530

    Bootstrapped frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Signup from "./components/Signup";
+import Signin from "./components/Signin";
+import Dashboard from "./components/Dashboard";
+import SendMoney from "./components/SendMoney";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/signin" element={<Signin />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/send" element={<SendMoney />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
 
-export default App
+export default App;
