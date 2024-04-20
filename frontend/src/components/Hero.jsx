09881921018b2fe6e8d96c2851bffcf49bdf74ea
hanzalahwaheed <hanzalah.w@gmail.com>
2024-04-20 15:11:00 +0400

    Hero subheading added

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -8,11 +8,10 @@ const Hero = () => {
       <div className="flex flex-col justify-center items-center">
         <div>
           <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white text-center">
-            Welcome to Finease
+            Welcome to FinEase
           </h1>
           <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-200">
-            Here at Flowbite we focus on markets where technology, innovation,
-            and capital can unlock long-term value and drive economic growth.
+            Finesse all you transactions and Payments with Ease.
           </p>
         </div>
         <Signin />
